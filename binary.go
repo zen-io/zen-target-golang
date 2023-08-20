14ca3ab041489c4c87d6bfa4f11ea4e993606bea
Tiago Posse <tiago.posse@hi.health>
2023-08-20 19:10:16 +0200

    use targetbuilder

--- binary.go
+++ binary.go
@@ -9,23 +9,23 @@ import (
 )
 
 type GolangBinaryConfig struct {
-	Name        string            `mapstructure:"name" desc:"Name for the target"`
-	Description string            `mapstructure:"desc" desc:"Target description"`
-	Labels      []string          `mapstructure:"labels" desc:"Labels to apply to the targets"` //
-	Deps        []string          `mapstructure:"deps" desc:"Build dependencies"`
-	PassEnv     []string          `mapstructure:"pass_env" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
-	SecretEnv   []string          `mapstructure:"secret_env" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
-	Env         map[string]string `mapstructure:"env" desc:"Key-Value map of static environment variables to be used"`
-	Tools       map[string]string `mapstructure:"tools" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
-	Visibility  []string          `mapstructure:"visibility" desc:"List of visibility for this target"`
-	Srcs        []string          `mapstructure:"srcs"`
-	Out         *string           `mapstructure:"out"`
-	Path        *string           `mapstructure:"path"`
-	Flags       string            `mapstructure:"flags"`
-	Toolchain   *string           `mapstructure:"toolchain"`
+	Name          string            `mapstructure:"name" zen:"yes" desc:"Name for the target"`
+	Description   string            `mapstructure:"desc" zen:"yes" desc:"Target description"`
+	Labels        []string          `mapstructure:"labels" zen:"yes" desc:"Labels to apply to the targets"` //
+	Deps          []string          `mapstructure:"deps" zen:"yes" desc:"Build dependencies"`
+	PassEnv       []string          `mapstructure:"pass_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
+	PassSecretEnv []string          `mapstructure:"secret_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
+	Env           map[string]string `mapstructure:"env" zen:"yes" desc:"Key-Value map of static environment variables to be used"`
+	Tools         map[string]string `mapstructure:"tools" zen:"yes" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
+	Visibility    []string          `mapstructure:"visibility" zen:"yes" desc:"List of visibility for this target"`
+	Srcs          []string          `mapstructure:"srcs"`
+	Out           *string           `mapstructure:"out"`
+	Path          *string           `mapstructure:"path"`
+	Flags         string            `mapstructure:"flags"`
+	Toolchain     *string           `mapstructure:"toolchain"`
 }
 
-func (gb GolangBinaryConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.Target, error) {
+func (gb GolangBinaryConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.TargetBuilder, error) {
 	if gb.Out == nil {
 		gb.Out = &gb.Name
 	}
@@ -39,41 +39,35 @@ func (gb GolangBinaryConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([
 		tools["golang"] = val
 	}
 
-	return []*zen_targets.Target{
-		zen_targets.NewTarget(
-			gb.Name,
-			zen_targets.WithSrcs(map[string][]string{"_srcs": gb.Srcs}),
-			zen_targets.WithOuts([]string{*gb.Out}),
-			zen_targets.WithVisibility(gb.Visibility),
-			zen_targets.WithTools(tools),
-			zen_targets.WithEnvVars(gb.Env),
-			zen_targets.WithPassEnv(gb.PassEnv),
-			zen_targets.WithTargetScript("build", &zen_targets.TargetScript{
-				Deps: gb.Deps,
-				Pre: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
-					if _, ok := target.Env["GOROOT"]; !ok {
-						target.Env["GOROOT"] = target.Tools["golang"]
-					}
-					target.Env["PATH"] = fmt.Sprintf("%s/bin:%s", target.Tools["golang"], target.Env["PATH"])
+	t := zen_targets.ToTarget(gb)
+	t.Srcs = map[string][]string{"_srcs": gb.Srcs}
+	t.Outs = []string{*gb.Out}
+	t.Scripts["build"] = &zen_targets.TargetBuilderScript{
+		Deps: gb.Deps,
+		Pre: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
+			if _, ok := target.Env["GOROOT"]; !ok {
+				target.Env["GOROOT"] = target.Tools["golang"]
+			}
+			target.Env["PATH"] = fmt.Sprintf("%s/bin:%s", target.Tools["golang"], target.Env["PATH"])
 
-					if _, ok := target.Env["GOBIN"]; !ok {
-						target.Env["GOBIN"] = fmt.Sprintf("%s/bin/go", target.Tools["golang"])
-					}
+			if _, ok := target.Env["GOBIN"]; !ok {
+				target.Env["GOBIN"] = fmt.Sprintf("%s/bin/go", target.Tools["golang"])
+			}
 
-					target.Env["ZEN_DEBUG_CMD"] = fmt.Sprintf("%s/bin/go build -o %s %s", target.Tools["golang"], filepath.Join(target.Cwd, *gb.Out), gb.Flags)
-					if gb.Path != nil {
-						target.Cwd = filepath.Join(target.Cwd, *gb.Path)
-					}
+			target.Env["ZEN_DEBUG_CMD"] = fmt.Sprintf("%s/bin/go build -o %s %s", target.Tools["golang"], filepath.Join(target.Cwd, *gb.Out), gb.Flags)
+			if gb.Path != nil {
+				target.Cwd = filepath.Join(target.Cwd, *gb.Path)
+			}
 
-					return nil
-				},
-				Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
-					if err := target.Exec([]string{"go", "mod", "tidy"}, "tidy"); err != nil {
-						return err
-					}
-					return target.Exec(strings.Split(target.Env["ZEN_DEBUG_CMD"], " "), "go build")
-				},
-			}),
-		),
-	}, nil
+			return nil
+		},
+		Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
+			if err := target.Exec([]string{"go", "mod", "tidy"}, "tidy"); err != nil {
+				return err
+			}
+			return target.Exec(strings.Split(target.Env["ZEN_DEBUG_CMD"], " "), "go build")
+		},
+	}
+
+	return []*zen_targets.TargetBuilder{t}, nil
 }
--- plugin.go
+++ plugin.go
@@ -9,23 +9,23 @@ import (
 )
 
 type GolangPluginConfig struct {
-	Name        string            `mapstructure:"name" desc:"Name for the target"`
-	Description string            `mapstructure:"desc" desc:"Target description"`
-	Labels      []string          `mapstructure:"labels" desc:"Labels to apply to the targets"` //
-	Deps        []string          `mapstructure:"deps" desc:"Build dependencies"`
-	PassEnv     []string          `mapstructure:"pass_env" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
-	SecretEnv   []string          `mapstructure:"secret_env" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
-	Env         map[string]string `mapstructure:"env" desc:"Key-Value map of static environment variables to be used"`
-	Tools       map[string]string `mapstructure:"tools" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
-	Visibility  []string          `mapstructure:"visibility" desc:"List of visibility for this target"`
-	Srcs        []string          `mapstructure:"srcs"`
-	Out         string            `mapstructure:"out"`
-	Path        *string           `mapstructure:"path"`
-	Flags       string            `mapstructure:"flags"`
-	Toolchain   *string           `mapstructure:"toolchain"`
+	Name          string            `mapstructure:"name" zen:"yes" desc:"Name for the target"`
+	Description   string            `mapstructure:"desc" zen:"yes" desc:"Target description"`
+	Labels        []string          `mapstructure:"labels" zen:"yes" desc:"Labels to apply to the targets"` //
+	Deps          []string          `mapstructure:"deps" zen:"yes" desc:"Build dependencies"`
+	PassEnv       []string          `mapstructure:"pass_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are part of the target hash"`
+	PassSecretEnv []string          `mapstructure:"secret_env" zen:"yes" desc:"List of environment variable names that will be passed from the OS environment, they are not used to calculate the target hash"`
+	Env           map[string]string `mapstructure:"env" zen:"yes" desc:"Key-Value map of static environment variables to be used"`
+	Tools         map[string]string `mapstructure:"tools" zen:"yes" desc:"Key-Value map of tools to include when executing this target. Values can be references"`
+	Visibility    []string          `mapstructure:"visibility" zen:"yes" desc:"List of visibility for this target"`
+	Srcs          []string          `mapstructure:"srcs"`
+	Out           string            `mapstructure:"out"`
+	Path          *string           `mapstructure:"path"`
+	Flags         string            `mapstructure:"flags"`
+	Toolchain     *string           `mapstructure:"toolchain"`
 }
 
-func (gp GolangPluginConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.Target, error) {
+func (gp GolangPluginConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([]*zen_targets.TargetBuilder, error) {
 	if gp.Out == "" {
 		gp.Out = gp.Name
 	}
@@ -33,55 +33,45 @@ func (gp GolangPluginConfig) GetTargets(tcc *zen_targets.TargetConfigContext) ([
 		gp.Out = fmt.Sprintf("%s.so", gp.Out)
 	}
 
-	tools := map[string]string{}
 	if gp.Toolchain != nil {
-		tools["golang"] = *gp.Toolchain
+		gp.Tools["golang"] = *gp.Toolchain
 	} else if val, ok := tcc.KnownToolchains["golang"]; !ok {
 		return nil, fmt.Errorf("golang toolchain is not configured")
 	} else {
-		tools["golang"] = val
+		gp.Tools["golang"] = val
 	}
 
-	opts := []zen_targets.TargetOption{
-		zen_targets.WithSrcs(map[string][]string{"_srcs": gp.Srcs}),
-		zen_targets.WithOuts([]string{gp.Out}),
-		zen_targets.WithVisibility(gp.Visibility),
-		zen_targets.WithTools(tools),
-		zen_targets.WithEnvVars(gp.Env),
-		zen_targets.WithTargetScript("build", &zen_targets.TargetScript{
-			Deps: gp.Deps,
+	t := zen_targets.ToTarget(gp)
+	t.Srcs = map[string][]string{"_srcs": gp.Srcs}
+	t.Outs = []string{gp.Out}
+	t.Scripts["build"] = &zen_targets.TargetBuilderScript{
+		Deps: gp.Deps,
 
-			Pre: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
-				if _, ok := target.Env["GOROOT"]; !ok {
-					target.Env["GOROOT"] = target.Tools["golang"]
-				}
-				target.Env["PATH"] = fmt.Sprintf("%s/bin:%s", target.Tools["golang"], target.Env["PATH"])
+		Pre: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
+			if _, ok := target.Env["GOROOT"]; !ok {
+				target.Env["GOROOT"] = target.Tools["golang"]
+			}
+			target.Env["PATH"] = fmt.Sprintf("%s/bin:%s", target.Tools["golang"], target.Env["PATH"])
 
-				if _, ok := target.Env["GOBIN"]; !ok {
-					target.Env["GOBIN"] = fmt.Sprintf("%s/bin/go", target.Tools["golang"])
-				}
+			if _, ok := target.Env["GOBIN"]; !ok {
+				target.Env["GOBIN"] = fmt.Sprintf("%s/bin/go", target.Tools["golang"])
+			}
 
-				target.Env["ZEN_DEBUG_CMD"] = fmt.Sprintf("%s/bin/go build -buildmode=plugin -o %s %s", target.Tools["golang"], filepath.Join(target.Cwd, gp.Out), gp.Flags)
-				if gp.Path != nil {
-					target.Cwd = filepath.Join(target.Cwd, *gp.Path)
-				}
+			target.Env["ZEN_DEBUG_CMD"] = fmt.Sprintf("%s/bin/go build -buildmode=plugin -o %s %s", target.Tools["golang"], filepath.Join(target.Cwd, gp.Out), gp.Flags)
+			if gp.Path != nil {
+				target.Cwd = filepath.Join(target.Cwd, *gp.Path)
+			}
 
-				return nil
-			},
-			Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
-				if err := target.Exec([]string{"go", "mod", "tidy"}, "tidy"); err != nil {
-					return err
-				}
+			return nil
+		},
+		Run: func(target *zen_targets.Target, runCtx *zen_targets.RuntimeContext) error {
+			if err := target.Exec([]string{"go", "mod", "tidy"}, "tidy"); err != nil {
+				return err
+			}
 
-				return target.Exec(strings.Split(target.Env["ZEN_DEBUG_CMD"], " "), "go build")
-			},
-		}),
+			return target.Exec(strings.Split(target.Env["ZEN_DEBUG_CMD"], " "), "go build")
+		},
 	}
 
-	return []*zen_targets.Target{
-		zen_targets.NewTarget(
-			gp.Name,
-			opts...,
-		),
-	}, nil
+	return []*zen_targets.TargetBuilder{t}, nil
 }
